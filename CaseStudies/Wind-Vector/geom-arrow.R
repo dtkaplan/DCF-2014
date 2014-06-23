16d92f63c6b8652d7a104a5e431a18b53433c9c5
Ethan Brown <ethancbrown@gmail.com>
2014-06-23 14:44:57 -0500

    geom arrow actually runs, though it doesn't do much useful...

--- CaseStudies/Wind-Vector/geom-arrow.R
+++ CaseStudies/Wind-Vector/geom-arrow.R
@@ -2,132 +2,86 @@ library(ggplot2)
 library(grid)
 library(proto)
 
-fieldGrob <- function(x, y, angle, length, size, 
-  colour="black", linetype=1, arrow=NULL){
-	
-   segmentsGrob(
-		x0= x - 0.5*length*cos(angle), 
-		y0= y - 0.5*length*sin(angle),
-		x1= x + 0.5*length*cos(angle), 
-		y1= y + 0.5*length*sin(angle), 
-		default.units="native",
-	        gp = gpar(col=colour, 
-                          lwd=size*ggplot2:::.pt, 
-                          lty=linetype, 
-                          lineend = "butt"), 
-	        arrow = arrow
-               )
+fieldGrob <- function(x, y, angle, length, size = 1, 
+                      colour="black", linetype=1, arrow=NULL, ...){
+    
+    segmentsGrob(
+        x0= x - 0.5*length*cos(angle), 
+        y0= y - 0.5*length*sin(angle),
+        x1= x + 0.5*length*cos(angle), 
+        y1= y + 0.5*length*sin(angle), 
+        default.units="native",
+        gp = gpar(col=colour, 
+            lwd=size*ggplot2:::.pt, 
+            lty=linetype, 
+            lineend = "butt"),
+        arrow = arrow(length = unit(length*4,"cm")),
+        )
 }
 
 
 
-g <-
-fieldGrob(0.5, 0.5, pi/6, 0.2,1,  col="blue")
-pushViewport(vp=viewport(width=1, height=1))
-grid.draw(g)
+## g <-
+## fieldGrob(0.5, 0.5, pi/6, 0.2,1,  col="blue", arrow = T)
+## pushViewport(vp=viewport(width=1, height=1))
+## grid.draw(g)
 
 
+GeomField <- proto(ggplot2:::Geom, {
 
-GeomField <- proto(Geom, {
-  draw <- function(., data, scales, coordinates, ...) 
-{
-	# draw the field grobs in relation to the data
+    draw <- function(., data, scales, coordinates, arrow = NULL,
+                     lineend = "butt", na.rm = FALSE, ...) {
+
+        data <- ggplot2:::remove_missing(data, na.rm = na.rm,
+                               c("x", "y", "angle", "length", "linetype", "size", "shape"),
+                               name = "geom_field")
+
+#        if (empty(data)) return(zeroGrob())
+
+        if (is.linear(coordinates)) {
+            return(with(coord_transform(coordinates, data, scales),
+                        fieldGrob(x, y, angle, length)))
+        }
+
+        data$group <- 1:nrow(data)
+        starts <- subset(data, select = c(-xend, -yend))
+        ends <- ggplot2:::rename(subset(data, select = c(-x, -y)), c("xend" = "x", "yend" = "y"),
+                       warn_missing = FALSE)
+
+        pieces <- rbind(starts, ends)
+        pieces <- pieces[order(pieces$group),]
+
+        ggplot2:::GeomPath$draw_groups(pieces, scales, coordinates, arrow = arrow, ...)
+    }
+    
+    objname <- "field" # name of the geom in lowercase. Must correspond to GeomField.
+    desc <- "Single line segments"
+    
+    default_stat <- function(.) ggplot2:::StatIdentity
+    required_aes <- c("x", "y") 
+    default_aes <- function(.) aes(colour="black", angle=pi/4, length=1, size=0.5, linetype=1)
+    guide_geom <- function(.) "path"
+    
+})
+
+geom_field <- function (mapping = NULL, data = NULL, stat = "identity",
+                        position = "identity", arrow = NULL, lineend = "butt", na.rm = FALSE, ...) {
+
+    GeomField$new(mapping = mapping, data = data, stat = stat,
+                  position = position, arrow = arrow, lineend = lineend, na.rm = na.rm, ...)
 }
- 
-  draw_legend <- function(., data, ...) {
-    # show a grob in the key that represents the data
-  }
- 
-  objname <- "field" # name of the geom in lowercase. Must correspond to GeomField.
-  desc <- "Single line segments"
- 
-  default_stat <- function(.) StatIdentity
-  required_aes <- c("x", "y", "angle", "length") 
-  default_aes <- function(.) aes(colour="black", angle=pi/4, length=1, size=0.5, linetype=1)
-  guide_geom <- function(.) "field"
- 
-  icon <- function(.) # a grob representing the geom for the webpage
- 
-  desc_params <- list( # description of the (optional) parameters of draw
-	 )
- 
-  seealso <- list(
-    geom_path = GeomPath$desc,
-    geom_segment = GeomPath$desc,
-    geom_line = GeomLine$desc
-  )
- 
-  examples <- function(.) {
-    # examples of the geom in use
-  }
-  
-})
 
 
 
-draw <- function(., data, scales, coordinates, arrow=NULL, ...) {
-  with(coordinates$transform(data, scales), 
-        fieldGrob(x, y, angle, length, size, 
-        col=colour, linetype, arrow)
-      )    
-  }
+scale_length <- function(..., expand = waiver()) {
+    continuous_scale("length", "length_c", identity, ...,
+                     expand = expand, guide = "none")
+}
 
 
+scale_length_manual <- function(..., values) {
+    manual_scale(length, values, ...)
+}
 
-  draw_legend <- function(., data, ...) {
-    data <- aesdefaults(data, .$default_aes(), list(...))
- 
-    with(data, ggname(.$my_name(),
-		fieldGrob(0.5, 0.5, angle, length, size,  col=colour, linetype) )
-	)
-  }
-
-
-
-geom_field <- GeomField$build_accessor()
-
-
-
-ScaleLength <- proto(ScaleContinuous, expr={
-  doc <- TRUE
-  common <- NULL
-  
-  new <- function(., name=NULL, limits=NULL, breaks=NULL, labels=NULL, trans = NULL, to = NULL) {
-    .super$new(., name=name, limits=limits, breaks=breaks, labels=labels, trans=trans, variable = "length", to = to)
-  }
-  
-  map <- function(., values) {
-    # rescale(values, .$to, .$input_set())
-   values
-  }
-  output_breaks <- function(.) .$map(.$input_breaks())
-  
-  objname <- "length"
-  desc <- "Length scale for continuous variable"
-  
-})
-
-
-
-ScaleLengthDiscrete <- proto(ScaleDiscrete, expr={
-  common <- NULL
-  objname <- "length_discrete"
-  .input <- .output <- "length"
-  desc <- "Length scale for discrete variables"
-  doc <- FALSE
- 
-  max_levels <- function(.) 11
-  output_set <- function(.) seq(1, 5, length=11)
- 
-})
-
-
-
- scale_length <- ScaleLength$build_accessor()
- scale_length_discrete <- ScaleLengthDiscrete$build_accessor()
-
-
-
-scale_length_manual <- ScaleManual$build_accessor(list(variable = "\"length\""))
 
 
