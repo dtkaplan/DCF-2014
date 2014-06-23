96fcc1750889a20a8ac80cd4ac981144c5714c1d
Ethan Brown <ethancbrown@gmail.com>
2014-06-23 17:16:04 -0500

    geom_field: arrows work, but no alpha channel

--- CaseStudies/Wind-Vector/geom-arrow.R
+++ CaseStudies/Wind-Vector/geom-arrow.R
@@ -13,34 +13,39 @@ GeomField <- proto(ggplot2:::Geom, {
 
         ## Calculate and add on "xend" and "yend"
         data$xend <- data$x + data$length*cos(data$angle)
-        data$yend <- data$y + data$length*sin(data$angle) 
+        data$yend <- data$y + data$length*sin(data$angle)
+        minsize <- which.min(data$length)
 
         if (is.linear(coordinates)) {
             return(with(coord_transform(coordinates, data, scales), {
+                
+                arrowsize <- with(data.frame(x, xend, y, yend)[minsize],
+                                  sqrt((x-xend)^2 + (y-yend)^2))
                 segmentsGrob(x, y, xend, yend, default.units="native",
-                                     ## gp = gpar(col=alpha(colour, alpha), fill = alpha(colour, alpha),
-                                     ##     lwd=size * .pt, lty=linetype, lineend = lineend),
-                                     arrow = arrow)
+                             gp = gpar(col= colour, fill = colour,
+                                 lwd=size * ggplot2:::.pt, lty=linetype, lineend = lineend),
+                             arrow = arrow(length = unit(arrowsize, "npc")))
+
             }
                         ))
             
         } else return(zeroGrob())
 
     }
-    
+
     objname <- "field" # name of the geom in lowercase. Must correspond to GeomField.
     desc <- "Single line segments"
-    
+
     default_stat <- function(.) ggplot2:::StatIdentity
     required_aes <- c("x", "y") 
     default_aes <- function(.) aes(colour="black", angle=pi/4, length=1, size=0.5, linetype=1)
     guide_geom <- function(.) "path"
-    
+
 })
 
 geom_field <- function (mapping = NULL, data = NULL, stat = "identity",
                         position = "identity", arrow = NULL, lineend = "butt", na.rm = FALSE, ...) {
-    
+
     GeomField$new(mapping = mapping, data = data, stat = stat,
                   position = position, arrow = arrow, lineend = lineend, na.rm = na.rm, ...)
 }
