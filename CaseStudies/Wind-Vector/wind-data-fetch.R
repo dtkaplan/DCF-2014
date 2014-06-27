f0a9f9784c035e678e4b3d4c87147cb3d217bd8c
Ethan Brown <ethancbrown@gmail.com>
2014-06-26 23:48:15 -0500

    clean up wind-data-fetch

--- CaseStudies/Wind-Vector/wind-data-fetch.R
+++ CaseStudies/Wind-Vector/wind-data-fetch.R
@@ -2,6 +2,7 @@
 lats <- c(8, 49)
 longs <- c(-140, -63)
 
+source("functions.R")
 
 may2014 <- getWindDay(2014, 5, 30, 12, lat.southnorth=lats, lon.westeast=longs)
 may1949 <- getWindDay(1949, 5, 30, 12, lat.southnorth=lats, lon.westeast=longs)
@@ -12,13 +13,6 @@ nov1948 <- getWindDay(1948, 11, 30, 12, lat.southnorth=lats, lon.westeast=longs)
 may2014_1949 <- rbind(may2014, may1949)
 may2014_nov2013 <- rbind(may2014, nov2013)
 nov2013_1948 <- rbind(nov2013, nov1948)
-
-scalef <- 0.15
-arrowunit <- 0.15
-
-may2014_1949 <- rbind(may2014, may1949)
-may2014_nov2013 <- rbind(may2014, nov2013)
-nov2014_1948 <- rbind(nov2013, nov1948)
 
 save(may2014_1949, may2014_nov2013, nov2013_1948, may2014, may1949, nov2013, file = "data/wind-2014-1949-comparisons.Rda")
 
