be331666b93e7e9c1bf3b1dc1dd3b9f8d5a5ffb4
Ethan Brown <ethancbrown@gmail.com>
2014-06-30 21:03:14 -0500

    wind data: convert date string to date format

--- CaseStudies/Wind-Vector/functions.R
+++ CaseStudies/Wind-Vector/functions.R
@@ -1,6 +1,7 @@
 library(reshape2)
 library(dplyr)
 library(RNCEP)
+library(lubridate)
 
 getWindDay <- function(year, month, day, hour = c(0, 6, 12, 18), 
                     lat.southnorth, lon.westeast) {
@@ -10,29 +11,40 @@ getWindDay <- function(year, month, day, hour = c(0, 6, 12, 18),
   
   if(length(chosenDay) > 1) stop("All of year, month, day, and hour must be specified and of length 1")
   
+  uv <- getWind(years = c(year, year), months = c(month, month),
+                lat.southnorth = lat.southnorth, lon.westeast = lon.westeast)
+
+  uv_filt <- filter(uv, date == ymd_h(chosenDay))  
+  
+  return(uv_filt)
+    
+}
+
+getWind <- function(years, months, lat.southnorth, lon.westeast) {
+  ## Retrieves & reshapes wind data by year, month
   
   u <- NCEP.gather(variable = 'uwnd.sig995', level = 'surface',
-                   months.minmax = c(month, month), years.minmax = c(year, year),
+                   months.minmax = months, years.minmax = years,
                    lat.southnorth = lat.southnorth, lon.westeast = lon.westeast)
-  
+
   v <- NCEP.gather(variable = 'vwnd.sig995', level = 'surface',
-                   months.minmax = c(month, month), years.minmax = c(year, year),
+                   months.minmax = months, years.minmax = years,
                    lat.southnorth = lat.southnorth, lon.westeast = lon.westeast)
+
   
-  umelt <- melt(u, varnames = c("lat", "long", "datestring"), value.name = "u")
-  vmelt <- melt(v, varnames = c("lat", "long", "datestring"), value.name = "v")
+  umelt <- melt(u, varnames = c("lat", "long", "date"), value.name = "u")
+  vmelt <- melt(v, varnames = c("lat", "long", "date"), value.name = "v")
   
-  uv <- inner_join(umelt, vmelt, by = c("lat", "long", "datestring"))
+  uv <- inner_join(umelt, vmelt, by = c("lat", "long", "date"))
   
   uv$long <- ifelse(uv$long > 180, uv$long - 360, uv$long) # Convert to negative notation
   
   ## Calculate speed and direction from u and v
   uv <- mutate(uv, speed = sqrt((u+v)^2),
-         angle = atan2(v, u))
+               angle = atan2(v, u),
+               date = ymd_h(date))
 
-  uv_filt <- filter(uv, datestring %in% chosenDay )  
-  
-  return(uv_filt)
+  return(uv)
     
 }
 
--- CaseStudies/Wind-Vector/wind-data-fetch.R
+++ CaseStudies/Wind-Vector/wind-data-fetch.R
@@ -4,6 +4,10 @@ longs <- c(-140, -63)
 
 source("functions.R")
 
+##################################################
+## Maps
+##################################################
+
 may2014 <- getWindDay(2014, 5, 30, 12, lat.southnorth=lats, lon.westeast=longs)
 may1949 <- getWindDay(1949, 5, 30, 12, lat.southnorth=lats, lon.westeast=longs)
 
@@ -16,4 +20,16 @@ nov2013_1948 <- rbind(nov2013, nov1948)
 
 save(may2014_1949, may2014_nov2013, nov2013_1948, may2014, may1949, nov2013, file = "data/wind-2014-1949-comparisons.Rda")
 
+
+##################################################
+## Time Series
+##################################################
+
+## Warren, WI
+lats2 <- c(45, 45)
+longs2 <- c(-92.5, -92.5)
+
+
+warren_wi <- getWind(years = c(1949, 2014), months = c(5, 5),
+                     lat.southnorth = lats2, lon.westeast = longs2)
 
